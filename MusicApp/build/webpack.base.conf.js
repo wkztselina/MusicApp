4c6759d25702505a3164117c7d99dcd142ca507d
wkztselina <1025276839@qq.com>
2017-08-22 14:11:48 +0800

    页面骨架开发
    vue-router和骨架

--- MusicApp/build/webpack.base.conf.js
+++ MusicApp/build/webpack.base.conf.js
@@ -22,20 +22,12 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'common':resolve('src/common')
     }
   },
   module: {
     rules: [
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
